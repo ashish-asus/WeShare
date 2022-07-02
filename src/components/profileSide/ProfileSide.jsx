84d45d6de24731a6a06321573b4a9f96d6a93cc4
ashish kataria <ashishkataria002@gmail.com>
2022-07-02 15:20:50 +0530

    third commit

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard/ProfileCard'
+import FollowersCard from '../FollowersCard/FollowersCard'
+import "./profileSide.css"
 const ProfileSide = () => {
   return (
     <div className="profileSide">
       <LogoSearch/>
+      <ProfileCard/>
+      <FollowersCard/>
     </div>
   )
 }
