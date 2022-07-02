37a382a29314b7c25dc8122eaf7737f8dd2777a5
ashish kataria <ashishkataria002@gmail.com>
2022-07-02 13:32:09 +0530

    second commit

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LogoSearch from '../LogoSearch/LogoSearch'
+const ProfileSide = () => {
+  return (
+    <div className="profileSide">
+      <LogoSearch/>
+    </div>
+  )
+}
+
+export default ProfileSide
