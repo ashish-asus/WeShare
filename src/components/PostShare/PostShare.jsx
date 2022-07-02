601268800c906cde6c45357b5dac654aad9a31a1
ashish kataria <ashishkataria002@gmail.com>
2022-07-02 15:55:09 +0530

    made followers Card

---
+++ src/components/PostShare/PostShare.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProfileImage from "../../img/profileImg.jpg";
+import './PostShare.css'
+const PostShare = () => {
+  return (
+    <div className='PostShare'>
+    <img src={ProfileImage} alt="" />
+      
+    </div>
+  )
+}
+
+export default PostShare
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Home.css'
 import ProfileSide from '../../components/profileSide/ProfileSide'
+import PostSide from '../../components/PostSide/PostSide'
 const Home = () => {
   return (
     <div className='Home'>
      <ProfileSide/>
-      <div className='postSide'>Posts</div>
+      <PostSide/>
       <div className='RightSide'>RightSide</div>
       
     </div>
