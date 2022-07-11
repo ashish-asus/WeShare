0b0d4ba6bb385bb9d4ef9cf2b55d4886223c40f2
ashish kataria <ashishkataria002@gmail.com>
2022-07-12 01:51:26 +0530

    made Trends Card

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './RightSide.css'
+import Home from "../../img/home.png";
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../TrendCard/TrendCard";
+import ShareModal from "../ShareModal/ShareModal";
+
+
+const RightSide = () => {
+  return (
+    <div>
+      RightSide
+    </div>
+  )
+}
+
+export default RightSide
